598caeb0b2039fed6aaeedcd1caef03bb25df9b4
goodnothin <dosadojohnmichael58@gmail.com>
2024-05-29 09:43:33 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    const offset = -currentSlide * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
